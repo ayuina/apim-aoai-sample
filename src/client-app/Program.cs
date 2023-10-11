8b0861a2f104237fe457e29ed49f7673dfc394d2
ayuina <ayumu.inaba@microsoft.com>
2023-10-11 18:47:25 +0900

    Func calling sample (#6)
    * temp
    
    * temp
    
    * function calling
    
    * add
    
    * dotnet compat

--- src/client-app/Program.cs
+++ src/client-app/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Text.Json;
+using Microsoft.Extensions.Configuration;
 
 using Azure;
 using Azure.AI.OpenAI;
-using dotenv.net;
-using dotenv.net.Utilities;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DependencyCollector;
 using Microsoft.ApplicationInsights.Extensibility;
 
+
 namespace client_app // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    internal partial class Program
     {
         static async Task Main(string[] args)
         {
@@ -27,79 +28,55 @@ namespace client_app // Note: actual namespace depends on the project name.
 
         private static async Task Run(string[] args)
         {
-            DotEnv.Load(new DotEnvOptions(probeForEnv: true));
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.secrets.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
 
-            var appiConfig = new TelemetryConfiguration(){
-                ConnectionString = EnvReader.GetStringValue("APPI_CONSTR"),
+            var appiConfig = new TelemetryConfiguration()
+            {
+                ConnectionString = config["APPI_CONSTR"],
                 TelemetryInitializers = { new MyTelemetryInitializer() }
             };
             DependencyTrackingTelemetryModule depModule = new DependencyTrackingTelemetryModule();
             depModule.Initialize(appiConfig);
 
-            var apimurl = string.Format("https://{0}.azure-api.net/", EnvReader.GetStringValue("APIM_NAME"));
-            var apimkey = EnvReader.GetStringValue("APIM_KEY");
-            await CallOpenAI(apimurl, apimkey);
+            var mode = config["mode"];
+            var loop = string.IsNullOrEmpty(config["loop"]) ? 1 : int.Parse(config["loop"]?? "");
+            var apimurl = string.Format("https://{0}.azure-api.net/", config["APIM_NAME"]);
+            var apimkey = config["APIM_KEY"] ?? "";
+            var aoaiurl = string.Format("https://{0}.openai.azure.com/", config["AOAI_NAME"]);
+            var aoaikey = config["AOAI_KEY"] ?? "";
 
-            // var aoaiurl = string.Format("https://{0}.openai.azure.com/", EnvReader.GetStringValue("AOAI_NAME"));
-            // var aoaikey = EnvReader.GetStringValue("AOAI_KEY");
-            // await CallOpenAI(aoaiurl, aoaikey);
+            switch (config["mode"]?.ToUpper())
+            {
+                case "APIM":
+                    await CallOpenAIHello(apimurl, apimkey, loop);
+                    break;
+                case "AOAI":
+                    await CallOpenAIHello(aoaiurl, aoaikey, loop);
+                    break;
+                case "FUNCCALL":
+                    await CallOpenAIFuncCalling(aoaiurl, aoaikey);
+                    break;
+                default:
+                    Console.WriteLine("Please specify mode as APIM or AOAI");
+                    break;
+            }
 
         }
 
-        //https://learn.microsoft.com/ja-jp/dotnet/api/overview/azure/ai.openai-readme?view=azure-dotnet-preview
-        private static async Task CallOpenAI(string endpoint, string apikey)
+        private static async Task<Response<ChatCompletions>> CallOpenAI(string endpoint, string apikey, ChatCompletionsOptions prompt)
         {
-            Console.WriteLine("Calling {0}", endpoint);
-
             var uri = new Uri(endpoint);
             var cred = new AzureKeyCredential(apikey);
             var client = new OpenAIClient(uri, cred);
 
-            var prompt = new ChatCompletionsOptions()
-            {
-                Messages = {
-                    new ChatMessage(ChatRole.System, @"You are an AI assistant that helps people find information."),
-                    new ChatMessage(ChatRole.User, @"hoge"),
-                    new ChatMessage(ChatRole.Assistant, @"Hello! How can I assist you today?"),
-                }
-            };
-
-            for(int i = 0; i < 1000; i++)
-            {
-                var response = await client.GetChatCompletionsAsync("g35t", prompt);
-                var raw = response.GetRawResponse();
-
-                Console.WriteLine("{0} {1} from {2} region", 
-                    raw.Status, 
-                    raw.ReasonPhrase, 
-                    raw.Headers.Where(h => h.Name == "x-ms-region" ).First().Value);
-           }
-
-            // Console.WriteLine("{0} {1}", response.GetRawResponse().Status, response.GetRawResponse().ReasonPhrase);
-            // response.GetRawResponse().Headers.OrderBy(h => h.Name).ToList().ForEach(h =>
-            // {
-            //     Console.WriteLine($"{h.Name}: {h.Value}");
-            // });
-
-            // Console.WriteLine();
-
-            // using (var writer = new Utf8JsonWriter(Console.OpenStandardOutput(), new JsonWriterOptions() { Indented = true }))
-            // {
-            //     JsonSerializer.Serialize(writer, response.Value);
-            // }
-
-            // Console.WriteLine();
-            // Console.WriteLine();
-
+            var response = await client.GetChatCompletionsAsync("g35t", prompt);
+            return response;
         }
 
     }
 
-    public class MyTelemetryInitializer : ITelemetryInitializer
-    {
-        public void Initialize(ITelemetry telemetry)
-        {
-            telemetry.Context.Cloud.RoleName = "client_app";
-        }
-    }
 }
