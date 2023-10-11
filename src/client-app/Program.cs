1bd26f291d1d801df95f1fa8934d10303e34daaa
Ayumu Inaba <ayumu.inaba@microsoft.com>
2023-10-11 12:41:42 +0900

    temp

--- src/client-app/Program.cs
+++ src/client-app/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Text.Json;
+using Microsoft.Extensions.Configuration;
 
 using Azure;
 using Azure.AI.OpenAI;
-using dotenv.net;
-using dotenv.net.Utilities;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DependencyCollector;
 using Microsoft.ApplicationInsights.Extensibility;
+
 
 namespace client_app // Note: actual namespace depends on the project name.
 {
@@ -27,70 +28,150 @@ namespace client_app // Note: actual namespace depends on the project name.
 
         private static async Task Run(string[] args)
         {
-            DotEnv.Load(new DotEnvOptions(probeForEnv: true));
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.secrets.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
 
-            var appiConfig = new TelemetryConfiguration(){
-                ConnectionString = EnvReader.GetStringValue("APPI_CONSTR"),
+            var appiConfig = new TelemetryConfiguration()
+            {
+                ConnectionString = config["APPI_CONSTR"],
                 TelemetryInitializers = { new MyTelemetryInitializer() }
             };
             DependencyTrackingTelemetryModule depModule = new DependencyTrackingTelemetryModule();
             depModule.Initialize(appiConfig);
 
-            var apimurl = string.Format("https://{0}.azure-api.net/", EnvReader.GetStringValue("APIM_NAME"));
-            var apimkey = EnvReader.GetStringValue("APIM_KEY");
-            await CallOpenAI(apimurl, apimkey);
+            var mode = config["mode"];
+            var loop = string.IsNullOrEmpty(config["loop"]) ? 1 : int.Parse(config["loop"]?? "");
+            var apimurl = string.Format("https://{0}.azure-api.net/", config["APIM_NAME"]);
+            var apimkey = config["APIM_KEY"] ?? "";
+            var aoaiurl = string.Format("https://{0}.openai.azure.com/", config["AOAI_NAME"]);
+            var aoaikey = config["AOAI_KEY"] ?? "";
 
-            // var aoaiurl = string.Format("https://{0}.openai.azure.com/", EnvReader.GetStringValue("AOAI_NAME"));
-            // var aoaikey = EnvReader.GetStringValue("AOAI_KEY");
-            // await CallOpenAI(aoaiurl, aoaikey);
+            switch (config["mode"]?.ToUpper())
+            {
+                case "APIM":
+                    await CallOpenAIHello(apimurl, apimkey, loop);
+                    break;
+                case "AOAI":
+                    await CallOpenAIHello(aoaiurl, aoaikey, loop);
+                    break;
+                case "FUNCCALL":
+                    await CallOpenAIFuncCalling(aoaiurl, aoaikey);
+                    break;
+                default:
+                    Console.WriteLine("Please specify mode as APIM or AOAI");
+                    break;
+            }
 
         }
 
-        //https://learn.microsoft.com/ja-jp/dotnet/api/overview/azure/ai.openai-readme?view=azure-dotnet-preview
-        private static async Task CallOpenAI(string endpoint, string apikey)
+        private static async Task CallOpenAIFuncCalling(string endpoint, string apikey)
         {
-            Console.WriteLine("Calling {0}", endpoint);
-
-            var uri = new Uri(endpoint);
-            var cred = new AzureKeyCredential(apikey);
-            var client = new OpenAIClient(uri, cred);
-
             var prompt = new ChatCompletionsOptions()
             {
                 Messages = {
                     new ChatMessage(ChatRole.System, @"You are an AI assistant that helps people find information."),
-                    new ChatMessage(ChatRole.User, @"hoge"),
-                    new ChatMessage(ChatRole.Assistant, @"Hello! How can I assist you today?"),
+                    new ChatMessage(ChatRole.User, @"Hello"),
+                },
+                FunctionCall = FunctionDefinition.Auto,
+                Functions = {
+                    new FunctionDefinition("get_weather_forecast"){
+                        Description = "指定された日付と場所の天気予報を取得します。",
+                        Parameters = BinaryData.FromString(@"{
+                            ""type"": ""object"",
+                            ""properties"": {
+                                ""location"": {
+                                    ""type"": ""string"",
+                                    ""description"": ""天気予報を取得する場所の地名""
+                                },
+                                ""date"": {
+                                    ""type"": ""string"",
+                                    ""description"": ""天気予報を取得する日時""
+                                }
+                            },
+                            ""required"": [""location"", ""datetime""]
+                        }"),
+                    },
+                }
+            };
+            
+            Console.WriteLine("Calling {0}", endpoint);
+            await foreach(var response in CallOpenAI(endpoint, apikey, prompt, 1))
+            {
+                var raw = response.GetRawResponse();
+
+                Console.WriteLine("{0} {1} from {2} region",
+                raw.Status,
+                raw.ReasonPhrase,
+                raw.Headers.Where(h => h.Name == "x-ms-region").First().Value);
+
+                raw.Headers.OrderBy(h => h.Name).ToList().ForEach(h =>
+                {
+                    Console.WriteLine($"{h.Name}: {h.Value}");
+                });
+
+                Console.WriteLine();
+
+                using (var writer = new Utf8JsonWriter(Console.OpenStandardOutput(), new JsonWriterOptions() { Indented = true }))
+                {
+                    JsonSerializer.Serialize(writer, response.Value);
+                }
+            };
+        }
+
+        private static async Task CallOpenAIHello(string endpoint, string apikey, int loop)
+        {
+            var prompt = new ChatCompletionsOptions()
+            {
+                Messages = {
+                    new ChatMessage(ChatRole.System, @"You are an AI assistant that helps people find information."),
+                    new ChatMessage(ChatRole.User, @"Hello"),
                 }
             };
 
-            for(int i = 0; i < 1000; i++)
+            Console.WriteLine("Calling {0}", endpoint);
+
+            await foreach(var response in CallOpenAI(endpoint, apikey, prompt, loop))
             {
-                var response = await client.GetChatCompletionsAsync("g35t", prompt);
                 var raw = response.GetRawResponse();
 
-                Console.WriteLine("{0} {1} from {2} region", 
-                    raw.Status, 
-                    raw.ReasonPhrase, 
-                    raw.Headers.Where(h => h.Name == "x-ms-region" ).First().Value);
-           }
+                Console.WriteLine("{0} {1} from {2} region",
+                raw.Status,
+                raw.ReasonPhrase,
+                raw.Headers.Where(h => h.Name == "x-ms-region").First().Value);
 
-            // Console.WriteLine("{0} {1}", response.GetRawResponse().Status, response.GetRawResponse().ReasonPhrase);
-            // response.GetRawResponse().Headers.OrderBy(h => h.Name).ToList().ForEach(h =>
-            // {
-            //     Console.WriteLine($"{h.Name}: {h.Value}");
-            // });
+                if (loop == 1)
+                {
+                    raw.Headers.OrderBy(h => h.Name).ToList().ForEach(h =>
+                    {
+                        Console.WriteLine($"{h.Name}: {h.Value}");
+                    });
 
-            // Console.WriteLine();
+                    Console.WriteLine();
 
-            // using (var writer = new Utf8JsonWriter(Console.OpenStandardOutput(), new JsonWriterOptions() { Indented = true }))
-            // {
-            //     JsonSerializer.Serialize(writer, response.Value);
-            // }
+                    using (var writer = new Utf8JsonWriter(Console.OpenStandardOutput(), new JsonWriterOptions() { Indented = true }))
+                    {
+                        JsonSerializer.Serialize(writer, response.Value);
+                    }
+                }
 
-            // Console.WriteLine();
-            // Console.WriteLine();
+            };
+        }
 
+        //https://learn.microsoft.com/ja-jp/dotnet/api/overview/azure/ai.openai-readme?view=azure-dotnet-preview
+        private static async IAsyncEnumerable<Response<ChatCompletions>> CallOpenAI(string endpoint, string apikey, ChatCompletionsOptions prompt, int loop)
+        {
+            var uri = new Uri(endpoint);
+            var cred = new AzureKeyCredential(apikey);
+            var client = new OpenAIClient(uri, cred);
+
+            for (int idx = 0; idx < loop; idx++)
+            {
+                var response = await client.GetChatCompletionsAsync("g35t", prompt);
+                yield return response;
+            }
         }
 
     }
